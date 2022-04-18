c1468fcbf38fccee5174f211e98f8f449f3e1390
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2022-04-18 22:50:16 +0200

    Renaming class CommandLine as MumbleCommandLine

--- src/main/java/fr/pederobien/mumble/commandline/MumbleClientCommandLine.java
+++ src/main/java/fr/pederobien/mumble/commandline/MumbleClientCommandLine.java
@@ -2,7 +2,7 @@ package fr.pederobien.mumble.commandline;
 
 import fr.pederobien.mumble.commandline.impl.BeginContext;
 
-public class CommandLine {
+public class MumbleClientCommandLine {
 
 	public static void main(String[] args) {
 		new BeginContext().initialize().start();
